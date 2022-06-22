cd330c25c90b784ca57c306b2bafe8c962a1007f
--get <merictabakoglu@gmail.com>
2022-06-22 21:27:51 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+using Console = Colorful.Console;
+
+Console.WriteAscii("Hello , World!");
+
+Console.WriteAscii("Hello, World!", Color.Red);
